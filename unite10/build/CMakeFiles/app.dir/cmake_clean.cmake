f73f5e4b523bad0a140b010baaf4e6900e31b02f
Powerpowername <1361581236@qq.com>
2025-04-12 21:50:38 +0800

    建立平行光照射模型

---
+++ unite10/build/CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app.dir/E_/lib_c/glad/src/glad.c.obj"
+  "CMakeFiles/app.dir/E_/lib_c/glad/src/glad.c.obj.d"
+  "CMakeFiles/app.dir/source/main.cpp.obj"
+  "CMakeFiles/app.dir/source/main.cpp.obj.d"
+  "CMakeFiles/app.dir/source/stb_image.cpp.obj"
+  "CMakeFiles/app.dir/source/stb_image.cpp.obj.d"
+  "bin/app.exe"
+  "bin/app.exe.manifest"
+  "bin/app.pdb"
+  "libapp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- unite10/source/main.cpp
+++ unite10/source/main.cpp
@@ -96,6 +96,19 @@ int main()
         -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  0.0f,
         -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f
     };
+    // positions all containers
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
 #pragma endregion
     // first, configure the cube's VAO (and VBO)
     unsigned int VBO, cubeVAO;
@@ -170,7 +183,17 @@ int main()
         object.setMat4("model", model);
         
         glBindVertexArray(cubeVAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        // glDrawArrays(GL_TRIANGLES, 0, 36);
+        for (size_t i = 0; i < 10; i++)
+        {
+            glm::mat4 model = glm::mat4 (1.0);
+            model = glm::translate(model,cubePositions[i]);
+            float angle = 20 * i;
+            model = glm::rotate(model,glm::radians(angle),glm::vec3(1.0,0.3,0.5));
+            object.setMat4("model",model);
+
+            glDrawArrays(GL_TRIANGLES,0,36);
+        }   
 
         // also draw the lamp object
         lightSoure.use();       
