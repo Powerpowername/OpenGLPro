65d15e0f4e406ec32c4ac55dcbd342d35600afdd
Powerpowername <1361581236@qq.com>
2025-04-19 21:11:43 +0800

    增加实例化操作

---
+++ unite21/source/main.cpp
@@ -0,0 +1,124 @@
+#include <winInit.hpp>
+#include <string>
+#include <cmath>
+#include <shader.hpp>
+#include <camera.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+// #include <fstream>
+using std::string;
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+
+int main()
+{
+    std::unique_ptr<GLFWwindow,windowDeleter> window(windowInit(3,3,SCR_WIDTH,SCR_HEIGHT));
+
+    if(window.get() == NULL)
+    {
+        std::cout<<"Failed to create GLFW window"<<std::endl;
+        glfwTerminate();
+    }
+    glfwMakeContextCurrent(window.get());
+    glfwSetFramebufferSizeCallback(window.get(),framebuffer_size_callback);
+    glfwSetCursorPosCallback(window.get(),mouse_callback);
+    glfwSetScrollCallback(window.get(), scroll_callback);
+    // tell GLFW to capture our mouse
+    glfwSetInputMode(window.get(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    // 添加GLAD初始化
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;  
+    }
+    Shader shader("../shaderSource/vertex.vs","../shaderSource/fragment.fs");
+#pragma region 顶点数据
+    float quadVertices[] = {
+        // 位置          // 颜色
+        -0.05f, 0.05f, 1.0f, 0.0f, 0.0f,
+        0.05f, -0.05f, 0.0f, 1.0f, 0.0f,
+        -0.05f, -0.05f, 0.0f, 0.0f, 1.0f,
+
+        -0.05f, 0.05f, 1.0f, 0.0f, 0.0f,
+        0.05f, -0.05f, 0.0f, 1.0f, 0.0f,
+        0.05f, 0.05f, 0.0f, 1.0f, 1.0f
+        };
+#pragma endregion
+    // first, configure the cube's VAO (and VBO)
+    // cube VAO
+    unsigned int cubeVAO, cubeVBO;
+    glGenVertexArrays(1, &cubeVAO);
+    glGenBuffers(1, &cubeVBO);
+    glBindVertexArray(cubeVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
+
+    glBindBuffer(GL_ARRAY_BUFFER,0);
+
+
+    glm::vec2 translations[100];
+    int index = 0;
+    float offset = 0.1f;
+    for (int y = -10; y < 10; y += 2)
+    {
+        for (int x = -10; x < 10; x += 2)
+        {
+            glm::vec2 translation;
+            translation.x = (float)x / 10.0f + offset;
+            translation.y = (float)y / 10.0f + offset;
+            translations[index++] = translation;
+        }
+    }
+    //创建第二个VBO，一个VAO可以管理多个VBO
+    unsigned int instanceVBO;
+    glGenBuffers(1,&instanceVBO);
+    glBindBuffer(GL_ARRAY_BUFFER,instanceVBO);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(glm::vec2) * 100,&translations[0],GL_STATIC_DRAW);
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(2);
+    glBindBuffer(GL_ARRAY_BUFFER,0);
+    glVertexAttribDivisor(2,1);
+    glBindVertexArray(0);
+    shader.use();
+
+    glEnable(GL_DEPTH_TEST);
+    while (!glfwWindowShouldClose(window.get()))
+    {
+
+        processInput(window.get());
+        glClearColor(0,0,0,1);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glBindVertexArray(cubeVAO);
+        glDrawArraysInstanced(GL_TRIANGLES,0,6,100);
+
+
+
+        glfwSwapBuffers(window.get());
+        glfwPollEvents();
+    }
+    glDeleteVertexArrays(1,&cubeVAO);
+    glDeleteBuffers(1,&cubeVBO);
+
+    glfwTerminate();
+    return 0;
+}
